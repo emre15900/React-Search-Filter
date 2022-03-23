914167137f5b7e82eaf042341ef91a237bf40600
emre15900 <79264023+emre15900@users.noreply.github.com>
2022-03-24 02:58:24 +0300

    api
    apiden gelen veriyi ekrana basiyoruz

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import "./app.css"
+import { Users } from "./users";
 
-function App(){
-    return(
+function App() {
+    return (
         <div className="app">
             <input type="text" placeholder="Search..." className="seacrh" />
-        <ul className="list">
-            <li className="listItem">John</li>
-            <li className="listItem">Jane</li>
-            <li className="listItem">Mike</li>
-        </ul>
+            <ul className="list">
+                {Users.map(user => (
+                    <li className="listItem">{user.first_name}</li>
+                ))}
+            </ul>
         </div>
     );
 }
