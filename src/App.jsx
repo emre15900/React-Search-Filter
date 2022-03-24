78694953895d77e6a3802c79800b1be5fb9ce2d3
emre15900 <79264023+emre15900@users.noreply.github.com>
2022-03-24 22:37:39 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,20 @@ import Table from "./Table";
 
 function App() {
   const [query, setQuery] = useState("");
+  const keys = ["first_name", "last_name", "email"];
 
   const search = (data) => {
-    return data.filter((item) => 
-    item.first_name.toLowerCase().includes(query) ||
-    item.last_name.toLowerCase().includes(query) ||
-    item.email.toLowerCase().includes(query)
+    return data.filter((item) =>
+      keys.some((key) => item[key].toLowerCase().includes(query))
     );
   };
-
+  
   return (
     <div className="app">
       <input
-        type="text"
-        placeholder="Search..."
         className="search"
-        onChange={(e) => setQuery(e.target.value)}
+        placeholder="Search..."
+        onChange={(e) => setQuery(e.target.value.toLowerCase())}
       />
       {/* 
       <ul className="list">
