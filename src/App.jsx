6bea74019445b0f79c023c35cdfc73ff296eb198
emre15900 <79264023+emre15900@users.noreply.github.com>
2022-03-24 03:14:30 +0300

    search

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,22 @@ import "./app.css"
 import { Users } from "./users";
 
 function App() {
+    const [query, setQuery] = useState("");
+
     return (
         <div className="app">
-            <input type="text" placeholder="Search..." className="seacrh" />
+
+            <input
+                type="text"
+                placeholder="Search..."
+                className="seacrh"
+                onChange={e => setQuery(e.target.value)}
+            />
+
             <ul className="list">
-                {Users.map(user => (
-                    <li className="listItem">{user.first_name}</li>
+                {Users.filter(user => user.first_name.toLowerCase().includes(query))
+                .map(user => (
+                    <li key={user.id} className="listItem">{user.first_name}</li>
                 ))}
             </ul>
         </div>
